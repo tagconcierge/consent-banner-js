1dbf585810653d8f6fc0742682909a42e7b76e64
Michał Frankiewicz <michal@frankiewicz.me>
2024-04-06 16:12:03 +0200

    update styles

--- json-to-css.js
+++ json-to-css.js
@@ -6,14 +6,24 @@ styles.forEach((style) => {
     const jsonStyle = require('./styles/' + style + '.json');
     let cssStyle = '';
     for (selector in jsonStyle) {
-        const parsedSelector = selector.replace(/([A-Z])/g, '-$1');
-        cssStyle += parsedSelector + " {\n";
-        for (prop in jsonStyle[selector]) {
-            cssStyle += "  " + prop + ": " + jsonStyle[selector][prop] + ";\n";
+        if (selector.startsWith('@media')) {
+            cssStyle += selector + " {\n";
+            for (const styleKey in jsonStyle[selector]) {
+                cssStyle += `  ${styleKey} {\n`;
+                for (const propertyKey in jsonStyle[selector][styleKey]) {
+                    cssStyle += `    ${propertyKey}: ${jsonStyle[selector][styleKey][propertyKey]};\n`;
+                }
+                cssStyle += `  }\n`;
+            }
+            cssStyle += `}\n\n`;
+        } else {
+            const parsedSelector = selector.replace(/([A-Z])/g, '-$1').toLowerCase();
+            cssStyle += `${parsedSelector} {\n`;
+            for (const propertyKey in jsonStyle[selector]) {
+                cssStyle += `  ${propertyKey}: ${jsonStyle[selector][propertyKey]};\n`;
+            }
+            cssStyle += `}\n\n`;
         }
-        cssStyle += "} \n\n";
     }
     fs.writeFileSync('./dist/consent-banner-js/' + process.env.npm_package_version + '/styles/' + style + '.css', cssStyle);
 });
-
-
